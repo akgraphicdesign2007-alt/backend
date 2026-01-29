7d5ea2bcacdacf1f1a016b4a705a7c437cf2f643
clientfoster <basumgarianand109@gmail.com>
2026-01-29 20:14:25 +0530

    config

--- server.js
+++ server.js
@@ -12,7 +12,20 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+const allowedOrigins = ['https://www.akdesigns.space', 'http://localhost:3000','https://akdesigns.space','https://api.akdesign.com'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
